7502f4d64b1db1e2d9ea05467d282236a624eeca
BrunoChVe <bruno.chihuanutec.edu.pe>
2025-04-17 03:00:28 +0000

    Primer commit - estructura inicial

---
+++ triangle_area.py
@@ -0,0 +1,6 @@
+### Triangle Area (cm2)
+print('Cálculo de Área de Triángulo')
+base = float(input('Ingrese la base en cm: '))
+height = float(input('Ingrese la altura en cm: '))
+triangle_area = (base * height)/2
+print(f'\033[1;34;48mEl Área del Triángulo es: \033[1;37;0m {triangle_area}')
